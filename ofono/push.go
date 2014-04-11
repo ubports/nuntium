19b6f93d4f9924977f05854d6b8e47489b466192
Sergio Schvezov <sergio.schvezov@canonical.com>
2014-04-11 15:41:05 -0300

    - Moving consts to separate file - Use the mms decoder to decode as much as possible - When decoding the remaining headers; all we care about is the application id

--- ofono/push.go
+++ ofono/push.go
@@ -24,139 +24,14 @@ package ofono
 import (
 	"errors"
 	"fmt"
+	"launchpad.net/nuntium/mms"
 	"reflect"
 )
 
 type PDU byte
 
-const (
-	CONNECT        PDU = 0x01
-	CONNECT_REPLY  PDU = 0x02
-	REDIRECT       PDU = 0x03
-	REPLY          PDU = 0x04
-	DISCONNECT     PDU = 0x05
-	PUSH           PDU = 0x06
-	CONFIRMED_PUSH PDU = 0x07
-	SUSPEND        PDU = 0x08
-	RESUME         PDU = 0x09
-	GET            PDU = 0x40
-	POST           PDU = 0x60
-)
-
-const (
-	WSP_PARAMETER_TYPE_Q                  = 0x00
-	WSP_PARAMETER_TYPE_CHARSET            = 0x01
-	WSP_PARAMETER_TYPE_LEVEL              = 0x02
-	WSP_PARAMETER_TYPE_TYPE               = 0x03
-	WSP_PARAMETER_TYPE_NAME_DEFUNCT       = 0x05
-	WSP_PARAMETER_TYPE_FILENAME_DEFUNCT   = 0x06
-	WSP_PARAMETER_TYPE_DIFFERENCES        = 0x07
-	WSP_PARAMETER_TYPE_PADDING            = 0x08
-	WSP_PARAMETER_TYPE_CONTENT_TYPE       = 0x09
-	WSP_PARAMETER_TYPE_START_DEFUNCT      = 0x0A
-	WSP_PARAMETER_TYPE_START_INFO_DEFUNCT = 0x0B
-	WSP_PARAMETER_TYPE_COMMENT_DEFUNCT    = 0x0C
-	WSP_PARAMETER_TYPE_DOMAIN_DEFUNCT     = 0x0D
-	WSP_PARAMETER_TYPE_MAX_AGE            = 0x0E
-	WSP_PARAMETER_TYPE_PATH_DEFUNCT       = 0x0F
-	WSP_PARAMETER_TYPE_SECURE             = 0x10
-	WSP_PARAMETER_TYPE_SEC                = 0x11
-	WSP_PARAMETER_TYPE_MAC                = 0x12
-	WSP_PARAMETER_TYPE_CREATION_DATE      = 0x13
-	WSP_PARAMETER_TYPE_MODIFICATION_DATE  = 0x14
-	WSP_PARAMETER_TYPE_READ_DATE          = 0x15
-	WSP_PARAMETER_TYPE_SIZE               = 0x16
-	WSP_PARAMETER_TYPE_NAME               = 0x17
-	WSP_PARAMETER_TYPE_FILENAME           = 0x18
-	WSP_PARAMETER_TYPE_START              = 0x19
-	WSP_PARAMETER_TYPE_START_INFO         = 0x1A
-	WSP_PARAMETER_TYPE_COMMENT            = 0x1B
-	WSP_PARAMETER_TYPE_DOMAIN             = 0x1C
-	WSP_PARAMETER_TYPE_PATH               = 0x1D
-	WSP_PARAMETER_TYPE_UNTYPED            = 0xFF
-)
-
-const (
-	ACCEPT                = 0x00
-	ACCEPT_CHARSET_1      = 0x01
-	ACCEPT_ENCODING_1     = 0x02
-	ACCEPT_LANGUAGE       = 0x03
-	ACCEPT_RANGES         = 0x04
-	AGE                   = 0x05
-	ALLOW                 = 0x06
-	AUTHORIZATION         = 0x07
-	CACHE_CONTROL_1       = 0x08
-	CONNECTION            = 0x09
-	CONTENT_BASE          = 0x0A
-	CONTENT_ENCODING      = 0x0B
-	CONTENT_LANGUAGE      = 0x0C
-	CONTENT_LENGTH        = 0x0D
-	CONTENT_LOCATION      = 0x0E
-	CONTENT_MD5           = 0x0F
-	CONTENT_RANGE_1       = 0x10
-	CONTENT_TYPE          = 0x11
-	DATE                  = 0x12
-	ETAG                  = 0x13
-	EXPIRES               = 0x14
-	FROM                  = 0x15
-	HOST                  = 0x16
-	IF_MODIFIED_SINCE     = 0x17
-	IF_MATCH              = 0x18
-	IF_NONE_MATCH         = 0x19
-	IF_RANGE              = 0x1A
-	IF_UNMODIFIED_SINCE   = 0x1B
-	LOCATION              = 0x1C
-	LAST_MODIFIED         = 0x1D
-	MAX_FORWARDS          = 0x1E
-	PRAGMA                = 0x1F
-	PROXY_AUTHENTICATE    = 0x20
-	PROXY_AUTHORIZATION   = 0x21
-	PUBLIC                = 0x22
-	RANGE                 = 0x23
-	REFERER               = 0x24
-	RETRY_AFTER           = 0x25
-	SERVER                = 0x26
-	TRANSFER_ENCODING     = 0x27
-	UPGRADE               = 0x28
-	USER_AGENT            = 0x29
-	VARY                  = 0x2A
-	VIA                   = 0x2B
-	WARNING               = 0x2C
-	WWW_AUTHENTICATE      = 0x2D
-	CONTENT_DISPOSITION_1 = 0x2E
-	X_WAP_APPLICATION_ID  = 0x2F
-	X_WAP_CONTENT_URI     = 0x30
-	X_WAP_INITIATOR_URI   = 0x31
-	ACCEPT_APPLICATION    = 0x32
-	BEARER_INDICATION     = 0x33
-	PUSH_FLAG             = 0x34
-	PROFILE               = 0x35
-	PROFILE_DIFF          = 0x36
-	PROFILE_WARNING_1     = 0x37
-	EXPECT                = 0x38
-	TE                    = 0x39
-	TRAILER               = 0x3A
-	ACCEPT_CHARSET        = 0x3B
-	ACCEPT_ENCODING       = 0x3C
-	CACHE_CONTROL_2       = 0x3D
-	CONTENT_RANGE         = 0x3E
-	X_WAP_TOD             = 0x3F
-	CONTENT_ID            = 0x40
-	SET_COOKIE            = 0x41
-	COOKIE                = 0x42
-	ENCODING_VERSION      = 0x43
-	PROFILE_WARNING       = 0x44
-	CONTENT_DISPOSITION   = 0x45
-	X_WAP_SECURITY        = 0x46
-	CACHE_CONTROL         = 0x47
-)
-
-type WSP interface {
-	DecodeField() (interface{}, error)
-}
-
 type PushPDU struct {
-	HeaderLength                             uint
+	HeaderLength                             uint64
 	ContentLength                            int
 	ApplicationId, EncodingVersion, PushFlag byte
 	ContentType                              string
@@ -164,13 +39,12 @@ type PushPDU struct {
 }
 
 type PushPDUDecoder struct {
-	data   []byte
-	offset int
+	mms.MMSDecoder
 }
 
 func NewDecoder(data []byte) *PushPDUDecoder {
 	decoder := new(PushPDUDecoder)
-	decoder.data = data
+	decoder.MMSDecoder.Data = data
 	return decoder
 }
 
@@ -182,116 +56,55 @@ func NewDecoder(data []byte) *PushPDUDecoder {
 // provided to and reported from the underlying transport. The Data field starts immediately after the Headers field and
 // ends at the end of the SDU.
 func (dec *PushPDUDecoder) Decode(pdu *PushPDU) (err error) {
-	if PDU(dec.data[1]) != PUSH {
-		return errors.New(fmt.Sprintf("%x != %x is not a push PDU", PDU(dec.data[1]), PUSH))
+	if PDU(dec.Data[1]) != PUSH {
+		return errors.New(fmt.Sprintf("%x != %x is not a push PDU", PDU(dec.Data[1]), PUSH))
 	}
 	// Move offset +tid +type = +2
-	dec.offset = 2
-	if pdu.HeaderLength, err = dec.decodeUintVar(); err != nil {
+	dec.Offset = 1
+	rValue := reflect.ValueOf(pdu).Elem()
+	if _, err = dec.ReadUintVar(&rValue, "HeaderLength"); err != nil {
 		return err
 	}
-	if pdu.ContentType, err = dec.decodeContentType(); err != nil {
+	if _, err = dec.ReadString(&rValue, "ContentType"); err != nil {
 		return err
 	}
+	dec.Offset++
 	if err = dec.decodeHeaders(pdu, int(pdu.HeaderLength)-(len(pdu.ContentType)+1)); err != nil {
 		return err
 	}
-	pdu.Data = dec.data[(pdu.HeaderLength + 3):]
+	pdu.Data = dec.Data[(pdu.HeaderLength + 3):]
 	return nil
-}
-
-// A UintVar is a variable lenght uint of up to 5 octects long where
-// more octects available are indicated with the most significant bit
-// set to 1
-func (dec *PushPDUDecoder) decodeUintVar() (uint, error) {
-	var val byte
-	var n int
-
-	for n = dec.offset; n < (dec.offset+5) && n < len(dec.data); n++ {
-		val = (val << 7) | (dec.data[n] & 0x7f)
-		if dec.data[n]&0x80 == 0 {
-			break
-		}
-	}
-	if dec.data[n]&0x80 == 1 {
-		return 0, errors.New(fmt.Sprintf("Could not decode uintvar from %x", dec.data[n:]))
-	}
-	dec.offset = n + 1
-	return uint(val), nil
-}
-
-func (dec *PushPDUDecoder) decodeContentType() (string, error) {
-	var err error
-	content, err := dec.DecodeField()
-	if err != nil {
-		return "", err
-	}
-	return reflect.ValueOf(content).String(), nil
 }
 
 func (dec *PushPDUDecoder) decodeHeaders(pdu *PushPDU, hdrLengthRemain int) error {
-	var n int
-	for n = dec.offset; n < (hdrLengthRemain + dec.offset); {
-		param := dec.data[n] & 0x7F
-		switch {
-		case param == X_WAP_APPLICATION_ID:
-			n++
-			pdu.ApplicationId = dec.data[n] & 0x7F
-			n++
-		case param == PUSH_FLAG:
-			n++
-			pdu.PushFlag = dec.data[n] & 0x7F
-			n++
-		case param == ENCODING_VERSION:
-			n++
-			pdu.EncodingVersion = dec.data[n] & 0x7F
-			n++
-		case param == CONTENT_LENGTH:
-			n++
-			pdu.ContentLength = int(dec.data[n] & 0x7F)
-			n++
+	rValue := reflect.ValueOf(pdu).Elem()
+	var err error
+	for ; dec.Offset < (hdrLengthRemain + dec.Offset); dec.Offset++ {
+		param := dec.Data[dec.Offset] & 0x7F
+		switch param {
+		case X_WAP_APPLICATION_ID:
+			_, err = dec.ReadInteger(&rValue, "ApplicationId")
+		case PUSH_FLAG:
+			_, err = dec.ReadShortInteger(&rValue, "PushFlag")
+		case ENCODING_VERSION:
+			dec.Offset++
+			pdu.EncodingVersion = dec.Data[dec.Offset] & 0x7F
+			dec.Offset++
+		case CONTENT_LENGTH:
+			_, err = dec.ReadInteger(&rValue, "ContentLength")
+		case X_WAP_INITIATOR_URI:
+			var v string
+			v, err = dec.ReadString(nil, "")
+			fmt.Println("Unsaved value decoded:", v)
 		default:
-			if pdu.ContentLength != 0 && pdu.ApplicationId != 0 {
-				return nil
-			} else {
-				fmt.Printf("Unhandled byte: %#0x\tdec: %d\tn: %d\tdec.offset: %d\n", param, param, n, dec.offset)
-				fmt.Println(pdu)
-				return fmt.Errorf("Unhandled %x, %d, %d", param, n, dec.offset)
-			}
+			err = fmt.Errorf("Unhandled header data %#x @%d", dec.Data[dec.Offset], dec.Offset)
 		}
+		if err != nil {
+			return fmt.Errorf("error while decoding %#x @%d: ", param, dec.Offset, err)
+		} else if pdu.ApplicationId != 0 {
+			return nil
+		}
+		
 	}
-	dec.offset = n
 	return nil
-}
-
-// decodeField decodes a header field following the following rules:
-//
-// WAP-230-WSP Section 8.4.1.2
-// 0-30 octet is followed by the indicated number of octets
-// 31 octet is followed by a uintvar
-// 32-127 value is a nul terminated string
-// 128-255 encoded 7bit value; no more data follows this octet
-func (dec *PushPDUDecoder) DecodeField() (interface{}, error) {
-	c := int(dec.data[dec.offset])
-	switch {
-	// Section 8.4.1.2 of WAP-230:
-	// Field is encoded in text format
-	case 32 < c && c < 127:
-		var (
-			text string
-			n    int
-			v    byte
-		)
-		for n, v = range dec.data[dec.offset:] {
-			if v != 0 {
-				continue
-			}
-			text = string(dec.data[dec.offset : dec.offset+n])
-			break
-		}
-		dec.offset = dec.offset + n + 1
-		return reflect.ValueOf(text).Interface(), nil
-	}
-
-	return nil, errors.New("Unhandled field type")
 }
---
+++ ofono/wsp_params.go
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2014 Canonical Ltd.
+ *
+ * Authors:
+ * Sergio Schvezov: sergio.schvezov@cannical.com
+ *
+ * This file is part of nuntium.
+ *
+ * nuntium is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * nuntium is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ofono
+
+//These are the WSP assigned numbers from Table 34. PDU Type Assignments -
+//Appendix A Assigned Numbers in WAP-230-WSP
+const (
+	CONNECT        PDU = 0x01
+	CONNECT_REPLY  PDU = 0x02
+	REDIRECT       PDU = 0x03
+	REPLY          PDU = 0x04
+	DISCONNECT     PDU = 0x05
+	PUSH           PDU = 0x06
+	CONFIRMED_PUSH PDU = 0x07
+	SUSPEND        PDU = 0x08
+	RESUME         PDU = 0x09
+	GET            PDU = 0x40
+	POST           PDU = 0x60
+)
+
+//These are the WSP assigned numbers from Table 38 . Well-Known Parameter 
+//Assignments - Appendix A Assigned Numbers in WAP-230-WSP
+const (
+	WSP_PARAMETER_TYPE_Q                  = 0x00
+	WSP_PARAMETER_TYPE_CHARSET            = 0x01
+	WSP_PARAMETER_TYPE_LEVEL              = 0x02
+	WSP_PARAMETER_TYPE_TYPE               = 0x03
+	WSP_PARAMETER_TYPE_NAME_DEFUNCT       = 0x05
+	WSP_PARAMETER_TYPE_FILENAME_DEFUNCT   = 0x06
+	WSP_PARAMETER_TYPE_DIFFERENCES        = 0x07
+	WSP_PARAMETER_TYPE_PADDING            = 0x08
+	WSP_PARAMETER_TYPE_CONTENT_TYPE       = 0x09
+	WSP_PARAMETER_TYPE_START_DEFUNCT      = 0x0A
+	WSP_PARAMETER_TYPE_START_INFO_DEFUNCT = 0x0B
+	WSP_PARAMETER_TYPE_COMMENT_DEFUNCT    = 0x0C
+	WSP_PARAMETER_TYPE_DOMAIN_DEFUNCT     = 0x0D
+	WSP_PARAMETER_TYPE_MAX_AGE            = 0x0E
+	WSP_PARAMETER_TYPE_PATH_DEFUNCT       = 0x0F
+	WSP_PARAMETER_TYPE_SECURE             = 0x10
+	WSP_PARAMETER_TYPE_SEC                = 0x11
+	WSP_PARAMETER_TYPE_MAC                = 0x12
+	WSP_PARAMETER_TYPE_CREATION_DATE      = 0x13
+	WSP_PARAMETER_TYPE_MODIFICATION_DATE  = 0x14
+	WSP_PARAMETER_TYPE_READ_DATE          = 0x15
+	WSP_PARAMETER_TYPE_SIZE               = 0x16
+	WSP_PARAMETER_TYPE_NAME               = 0x17
+	WSP_PARAMETER_TYPE_FILENAME           = 0x18
+	WSP_PARAMETER_TYPE_START              = 0x19
+	WSP_PARAMETER_TYPE_START_INFO         = 0x1A
+	WSP_PARAMETER_TYPE_COMMENT            = 0x1B
+	WSP_PARAMETER_TYPE_DOMAIN             = 0x1C
+	WSP_PARAMETER_TYPE_PATH               = 0x1D
+	WSP_PARAMETER_TYPE_UNTYPED            = 0xFF
+)
+
+//These are the WSP assigned numbers from Table 39 . Header Field Name
+//Assignments - Appendix A Assigned Numbers in WAP-230-WSP
+const (
+	ACCEPT                = 0x00
+	ACCEPT_CHARSET_1      = 0x01
+	ACCEPT_ENCODING_1     = 0x02
+	ACCEPT_LANGUAGE       = 0x03
+	ACCEPT_RANGES         = 0x04
+	AGE                   = 0x05
+	ALLOW                 = 0x06
+	AUTHORIZATION         = 0x07
+	CACHE_CONTROL_1       = 0x08
+	CONNECTION            = 0x09
+	CONTENT_BASE          = 0x0A
+	CONTENT_ENCODING      = 0x0B
+	CONTENT_LANGUAGE      = 0x0C
+	CONTENT_LENGTH        = 0x0D
+	CONTENT_LOCATION      = 0x0E
+	CONTENT_MD5           = 0x0F
+	CONTENT_RANGE_1       = 0x10
+	CONTENT_TYPE          = 0x11
+	DATE                  = 0x12
+	ETAG                  = 0x13
+	EXPIRES               = 0x14
+	FROM                  = 0x15
+	HOST                  = 0x16
+	IF_MODIFIED_SINCE     = 0x17
+	IF_MATCH              = 0x18
+	IF_NONE_MATCH         = 0x19
+	IF_RANGE              = 0x1A
+	IF_UNMODIFIED_SINCE   = 0x1B
+	LOCATION              = 0x1C
+	LAST_MODIFIED         = 0x1D
+	MAX_FORWARDS          = 0x1E
+	PRAGMA                = 0x1F
+	PROXY_AUTHENTICATE    = 0x20
+	PROXY_AUTHORIZATION   = 0x21
+	PUBLIC                = 0x22
+	RANGE                 = 0x23
+	REFERER               = 0x24
+	RETRY_AFTER           = 0x25
+	SERVER                = 0x26
+	TRANSFER_ENCODING     = 0x27
+	UPGRADE               = 0x28
+	USER_AGENT            = 0x29
+	VARY                  = 0x2A
+	VIA                   = 0x2B
+	WARNING               = 0x2C
+	WWW_AUTHENTICATE      = 0x2D
+	CONTENT_DISPOSITION_1 = 0x2E
+	X_WAP_APPLICATION_ID  = 0x2F
+	X_WAP_CONTENT_URI     = 0x30
+	X_WAP_INITIATOR_URI   = 0x31
+	ACCEPT_APPLICATION    = 0x32
+	BEARER_INDICATION     = 0x33
+	PUSH_FLAG             = 0x34
+	PROFILE               = 0x35
+	PROFILE_DIFF          = 0x36
+	PROFILE_WARNING_1     = 0x37
+	EXPECT                = 0x38
+	TE                    = 0x39
+	TRAILER               = 0x3A
+	ACCEPT_CHARSET        = 0x3B
+	ACCEPT_ENCODING       = 0x3C
+	CACHE_CONTROL_2       = 0x3D
+	CONTENT_RANGE         = 0x3E
+	X_WAP_TOD             = 0x3F
+	CONTENT_ID            = 0x40
+	SET_COOKIE            = 0x41
+	COOKIE                = 0x42
+	ENCODING_VERSION      = 0x43
+	PROFILE_WARNING       = 0x44
+	CONTENT_DISPOSITION   = 0x45
+	X_WAP_SECURITY        = 0x46
+	CACHE_CONTROL         = 0x47
+)
+
