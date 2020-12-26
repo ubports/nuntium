7aa5669e4a53f5b6a5bc871662af7c11a1ea46f3
jEzEk <jezek@chicki.sk>
2020-12-26 17:59:02 +0100

    nuntium-inject-push push payload swap for split-join vesion, remove test

--- cmd/nuntium-inject-push/push_test.go
+++ cmd/nuntium-inject-push/push_test.go
@@ -1,12 +1,1 @@
 package main
-
-import (
-	"reflect"
-	"testing"
-)
-
-func TestMNotificationIndSplitted(t *testing.T) {
-	if !reflect.DeepEqual(mNotificationInd, mNotificationIndSplitted) {
-		t.Errorf("Not equal")
-	}
-}
